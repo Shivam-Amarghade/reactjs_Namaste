bba04a39936980854ce5798729e7374b07ac5810
SHIVAM AMARGHADE <26050048@mponline.demo.gov.in>
2026-06-11 16:36:39 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-const parent =React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"main hu bacha"),React.createElement("h1",{},"main hu bacha")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"main hu bacha"),React.createElement("h1",{},"main hu bacha")])]);
-const heading =React.createElement("h1",{ style: {
-      background: "black",
-      color: "white"
-    }},"hello Bhai shivam  kese  ho tum bhai ");
- const root=ReactDOM.createRoot(document.getElementById("root"));
- console.log(heading) //object
 
- root.render(parent);//reader method conveting the heading object into html tag 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "main hu bacha"),
+    React.createElement("h1", {}, "main hu bacha"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "main hu bacha"),
+    React.createElement("h1", {}, "main hu bacha"),
+  ]),
+]);
+const heading = React.createElement(
+  "h1",
+  {
+    style: {
+      background: "black",
+      color: "white",
+    },
+  },
+  "hello Bhai shivam  kese  ho tum bhai ",
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading); //object
+
+root.render(parent); //reader method conveting the heading object into html tag
