c5a1f7b1bf9ef949b0720b69c8fd1084d8fbd7fd
SHIVAM AMARGHADE <26050048@mponline.demo.gov.in>
2026-06-11 11:23:35 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const parent =React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"main hu bacha"),React.createElement("h1",{},"main hu bacha")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"main hu bacha"),React.createElement("h1",{},"main hu bacha")])]);
+const heading =React.createElement("h1",{ style: {
+      background: "black",
+      color: "white"
+    }},"hello Bhai shivam  kese  ho tum bhai ");
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ console.log(heading) //object
+
+ root.render(parent);//reader method conveting the heading object into html tag 
